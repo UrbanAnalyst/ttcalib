f1b778cd9dd8a1173140249e682e15a09ae627c5
mpadge <mark.padgham@email.com>
2022-10-16 12:09:54 +0200

    finish ttcalib_penalties fn

--- R/calibrate-penalties.R
+++ R/calibrate-penalties.R
@@ -82,15 +82,53 @@ ttcalib_streetnet_batch <- function (path,
 #' Calculate standard error against 'uber' data for all weighted networks
 #' generated via \link{ttcalib_streetnet_batch}.
 #'
+#' @param path_results Path to directory holding main OSM network data,
+#' including a sub-directory with output of \link{ttcalib_streetnet_batch}.
+#' @param path_uberdata Path to Uber movement data.
 #' @inheritParams ttcalib_uberdata
 #'
 #' @export
-ttcalib_penalties <- function (path, city, hours = NULL) {
+ttcalib_penalties <- function (path_results, path_uberdata, city, hours = NULL) {
 
-    geodata <- ttcalib_geodata (path = path, city = city)
-    uberdata <- ttcalib_uberdata (path = path, hours = hours, city = city)
+    requireNamespace ("pbapply")
 
-    path_b <- find_batch_result_dir (path)
+    geodata <- ttcalib_geodata (path = path_uberdata, city = city)
+    uberdata <- ttcalib_uberdata (path = path_uberdata, hours = hours, city = city)
+
+    path_b <- find_batch_result_dir (path_results)
+
+    flist <- list.files (
+        path_b,
+        full.names = TRUE,
+        pattern = "\\_tl.*\\_tu.*\\_dlim.*\\.Rds"
+    )
+
+    res <- pbapply::pblapply (flist, function (f) {
+
+        tl <- regmatches (f, regexpr ("\\_tl[0-9]+", f))
+        tl <- as.integer (gsub ("^\\_tl", "", tl)) / 10
+        tu <- regmatches (f, regexpr ("\\_tu[0-9]+", f))
+        tu <- as.integer (gsub ("^\\_tu", "", tu)) / 10
+
+        graph <- fst::read_fst (f)
+        class (graph) <- c ("dodgr_streetnet_sc", class (graph))
+        attr (graph, "left_side") <- FALSE
+        attr (graph, "wt_profile") <- "motorcar"
+        attr (graph, "turn_penalty") <- tu
+
+        dat <- ttcalib_traveltimes (graph, geodata, uberdata)
+
+        dat <- dat [which (is.finite (dat$m4ra)), ]
+        mod <- summary (lm (log10 (dat$uber) ~ log10 (dat$m4ra)))
+
+        return (c (
+            traffic_lights = tl,
+            turn = tu,
+            r2 = mod$r.squared,
+            residuals = sum (mod$residuals ^ 2)))
+    })
+
+    return (data.frame (do.call (rbind, res)))
 }
 
 find_batch_result_dir <- function (path) {
